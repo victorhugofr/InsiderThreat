a9f935b200f087d94e5d355a23f8683c6d342d23
Victor Hugo <vale1c@ufrn.edu.br>
2018-11-22 12:37:39 -0300

    responsabilidade transferida para a tree

--- src/DataBase.java
+++ src/DataBase.java
@@ -7,52 +7,19 @@ public class DataBase {
 		users=new ArrayList<Tree>();
 	}
 	
-	public void verifica(Action teste) {
+	public void verifica(Action teste) { // VERIFICA SE JA EXISTE UMA ÁRVORE COM AQUELE USER
 		Node aux;
-		Node aux2;
-		Node aux3,aux4;
 		boolean existe= false;
 		for(int i=0;i<users.size();i++) {
 			aux=users.get(i).search(users.get(i).getRoot(),teste.getUser()); 
 			if(aux!=null) {//procura usuario
 				existe=true;
 				aux.getHist()[Integer.parseInt(teste.getDateString().substring(11, 13))]++;
-				aux2=users.get(i).search(aux,teste.getDateString()); 
-				if(aux2!=null) {//procura data
-					aux.getHist()[Integer.parseInt(teste.getDateString().substring(11, 13))]++;
-					aux3=users.get(i).search(aux,teste.getPc());
-					if(aux3!=null) {//procura PC
-						aux3.getHist()[Integer.parseInt(teste.getDateString().substring(11, 13))]++;
-						aux4=users.get(i).search(aux,teste.getActivity());
-						if(aux4!=null) {//procura atividade
-							aux4.getHist()[Integer.parseInt(teste.getDateString().substring(11, 13))]++;
-						}else {
-							Node activity=new Node(teste.getActivity());
-							activity.getHist()[Integer.parseInt(teste.getDateString().substring(11, 13))]++;
-							aux2.getChilds().add(activity);
-						}
-					}else {
-						Node PC= new Node(teste.getPc());
-						Node activity=new Node(teste.getActivity());
-						PC.getChilds().add(activity);
-						PC.getHist()[Integer.parseInt(teste.getDateString().substring(11, 13))]++;
-						aux.getChilds().add(PC);
-					}
-				}else {
-					Node data=new Node(teste.getDateString());
-					Node PC= new Node(teste.getPc());
-					Node activity=new Node(teste.getActivity());
-					PC.getChilds().add(activity);
-					data.getChilds().add(PC);
-					
-					data.getHist()[Integer.parseInt(teste.getDateString().substring(11, 13))]++;
-					PC.getHist()[Integer.parseInt(teste.getDateString().substring(11, 13))]++;
-					aux.getChilds().add(data);
-				}
+				users.get(i).refresh(teste);
 			}
 		}
 		
-		if(existe==false) {
+		if(existe==false) {// CASO NAO EXISTA, CRIE E ADICIONE SUAS RAIZES
 			Node id= new Node(teste.getUser());
 			Node data=new Node(teste.getDateString());
 			Node PC= new Node(teste.getPc());
@@ -75,7 +42,7 @@ public class DataBase {
 		}
 	}
 	
-	public void print() {
+	public void print() { // print das arvores
 		Node aux,aux2,aux3,aux4;
 		int i,j,z,h;
 		for(i=0;i<users.size();i++) {
--- src/Tree.java
+++ src/Tree.java
@@ -21,6 +21,42 @@ public class Tree {
 	        return returnNode;
 	    }
 	 
+	 public void refresh(Action teste) {// VERIFICA SE JA EXISTE AS FOLHAS(DATA,PC,ATIVIDADE)
+		 Node aux2,aux,aux3,aux4;
+		 aux=search(getRoot(),teste.getUser());
+		 aux2=search(aux,teste.getDateString()); 
+			if(aux2!=null) {//procura data
+				aux.getHist()[Integer.parseInt(teste.getDateString().substring(11, 13))]++;
+				aux3=search(aux,teste.getPc());
+				if(aux3!=null) {//procura PC
+					aux3.getHist()[Integer.parseInt(teste.getDateString().substring(11, 13))]++;
+					aux4=search(aux,teste.getActivity());
+					if(aux4!=null) {//procura atividade
+						aux4.getHist()[Integer.parseInt(teste.getDateString().substring(11, 13))]++;
+					}else {
+						Node activity=new Node(teste.getActivity());
+						activity.getHist()[Integer.parseInt(teste.getDateString().substring(11, 13))]++;
+						aux2.getChilds().add(activity);
+					}
+				}else {
+					Node PC= new Node(teste.getPc());
+					Node activity=new Node(teste.getActivity());
+					PC.getChilds().add(activity);
+					PC.getHist()[Integer.parseInt(teste.getDateString().substring(11, 13))]++;
+					aux.getChilds().add(PC);
+				}
+			}else {
+				Node data=new Node(teste.getDateString());
+				Node PC= new Node(teste.getPc());
+				Node activity=new Node(teste.getActivity());
+				PC.getChilds().add(activity);
+				data.getChilds().add(PC);
+				
+				data.getHist()[Integer.parseInt(teste.getDateString().substring(11, 13))]++;
+				PC.getHist()[Integer.parseInt(teste.getDateString().substring(11, 13))]++;
+				aux.getChilds().add(data);
+			}
+		}
 	public void setRoot(Node root) {
 		this.root=root;
 	}
